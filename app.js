a3ec0dc38e48f52d31c7975c3f739fe88d132865
Alice <alicenjoroge707@gmail.com>
2020-02-19 06:11:37 +0300

    creating a server

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ var newMod = require("./new");
 newMod.log("message here");
 newMod.timer();
 console.log(newMod.ninjas(["Alice", "Humphrey"]));
+
+/**
+ * creating a server
+ */
+var http = require("http");
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { "content-Type": "text/plain" });
+  res.end("hey there");
+});
+
+server.listen(3000, "127.0.0.1");
+console.log("this is the server");
